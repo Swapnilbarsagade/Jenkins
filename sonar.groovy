3e3114c1c8c3a848e41a0219e402176a9d1e964c
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2025-01-17 19:19:42 +0530

    qwerty

--- sonar.groovy
+++ sonar.groovy
@@ -1,7 +1,5 @@
 pipeline {
-    agent {
-        label 'dummy'
-    }
+    agent any
     stages {
         stage('Pull') {
             steps {
@@ -17,13 +15,13 @@ pipeline {
                 sudo apt install -y maven unzip
 
                 # Remove existing Tomcat if it exists
-                if [ -d "apache-tomcat-9.0.97" ]; then
-                    sudo rm -rf apache-tomcat-9.0.97
+                if [ -d "apache-tomcat-9.0.98" ]; then
+                    sudo rm -rf apache-tomcat-9.0.98
                 fi
         
                 # Download and unzip Tomcat
-                sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.zip
-                sudo unzip apache-tomcat-9.0.97.zip
+                sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.98.zip
+                sudo unzip apache-tomcat-9.0.98.zip
 
                 # Build the project
                 mvn clean package
@@ -36,10 +34,10 @@ pipeline {
                 withSonarQubeEnv('SonarQube') { // Replace 'SonarQube' with your SonarQube configuration name
                     sh '''
                     mvn sonar:sonar \
-                        -Dsonar.projectKey=studentapp \
-                        -Dsonar.host.url=http://43.202.110.30:9000 \
-                        -Dsonar.login=$SONAR_AUTH_TOKEN
-                    '''
+                    -Dsonar.projectKey=studentapp \
+                    -Dsonar.host.url=http://3.38.190.6:9000 \
+                    -Dsonar.login=d00aedb0f9d6fc51aaaae16c59d5fb595395a6ae
+                                        '''
                 }
                 echo "Testing completed"
             }
@@ -48,10 +46,10 @@ pipeline {
             steps {
                 sh '''
                 # Deploy the application to Tomcat
-                mv target/*.war apache-tomcat-9.0.97/webapps/student.war
+                mv target/*.war apache-tomcat-9.0.98/webapps/student.war
 
                 # Start Tomcat server
-                bash apache-tomcat-9.0.97/bin/catalina.sh start
+                bash apache-tomcat-9.0.98/bin/catalina.sh start
                 '''
                 echo "Deployment completed"
             }
